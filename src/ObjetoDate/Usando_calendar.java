831a7caff5b7a6280c938a559e1594783aa85b73
Yuri <glleciosyuri@gmail.com>
2023-10-29 16:10:42 -0300

    add aula calendar

--- src/ObjetoDate/Usando_calendar.java
+++ src/ObjetoDate/Usando_calendar.java
@@ -1,4 +1,10 @@
 package ObjetoDate;
 
+import java.util.Calendar;
+
 public class Usando_calendar {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
+    }
 }
