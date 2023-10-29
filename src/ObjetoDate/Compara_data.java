4c3ff95c388b4f821135616961ddf24a388999aa
Yuri <glleciosyuri@gmail.com>
2023-10-29 18:40:58 -0300

    add aula comparar data

---
+++ src/ObjetoDate/Compara_data.java
@@ -0,0 +1,19 @@
+package ObjetoDate;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Compara_data {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyyy");
+        Date dataBoleto = simpleDateFormat.parse("10/04/2024");
+        Date dataAtual = simpleDateFormat.parse("29/10/2023");
+
+        if (dataBoleto.after(dataAtual)){
+            System.out.println("seu boleto esta em dia. PARABENS!");
+        } else {
+            System.out.println("seu boleto está vencido");
+        }
+    }
+}
