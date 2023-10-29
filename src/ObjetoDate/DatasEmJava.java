56654618a767235b872da1c57917ec466f55e2dc
Yuri <glleciosyuri@gmail.com>
2023-10-29 12:50:39 -0300

    Conhecendo o objeto date

--- src/ObjetoDate/DatasEmJava.java
+++ src/ObjetoDate/DatasEmJava.java
@@ -1,5 +1,14 @@
 package ObjetoDate;
 
-public class DatasEmJava {
+import java.util.Date;
 
+public class DatasEmJava {
+    public static void main(String[] args) {
+        Date data = new Date();
+        System.out.println("Mês do ano: " + data.getMonth());
+        System.out.println("Dia do mês: " + data.getDate());
+        System.out.println("Dia da semana: " + data.getDay());
+        System.out.println("Hora do dia: " + data.getHours());
+        System.out.println("Minuto do dia: " + data.getMinutes());
+    }
 }
