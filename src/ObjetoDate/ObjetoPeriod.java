d80b2e76371740cfb0c7b4911e56e627dd861e88
DESKTOP-YURI\Maxima <glleciosyuri@gmail.com>
2023-11-03 07:46:26 -0300

    ADD AULA

--- src/ObjetoDate/ObjetoPeriod.java
+++ src/ObjetoDate/ObjetoPeriod.java
@@ -2,6 +2,7 @@ package ObjetoDate;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
 public class ObjetoPeriod {
@@ -9,7 +10,10 @@ public class ObjetoPeriod {
         LocalDate dataAntiga = LocalDate.of(2022,9,1);
         LocalDate dataNova = LocalDate.of(2023,11,3);
 
-        Period periodo = Period.between(dataAntiga,dataNova);
-        System.out.println(periodo);
+        Period periodo = Period.between(dataAntiga, dataNova);
+
+        System.out.println(periodo.getDays());
+        System.out.println(periodo.getMonths());
+        System.out.println(periodo.getYears());
     }
 }
